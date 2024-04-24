204588833e7fabd816a1d8196b80bc80b47b0fca
WP2-AkmalSidik <akmalsidik18@gmail.com>
2024-04-24 15:27:12 +0700

    Steo ke 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,5 +52,5 @@ dependencies {
     implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
     implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
 
-    // TODO: Tambahkan Library TensorFlow Lite
+
 }
